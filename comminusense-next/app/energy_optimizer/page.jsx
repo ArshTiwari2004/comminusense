66727ac4c4e28afda852a55ff2cba23440ab1e43
Anshit <anshit12005@gmail.com>
2025-10-08 22:37:53 +0530

    update

--- comminusense-next/app/energy_optimizer/page.jsx
+++ comminusense-next/app/energy_optimizer/page.jsx
@@ -28,7 +28,7 @@ export default function EnergyOptimizer() {
     setLoading(true);
     setResult(null);
 
-    const res = await fetch("http://127.0.0.1:8000/predict", {
+    const res = await fetch("https://comminusense.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
